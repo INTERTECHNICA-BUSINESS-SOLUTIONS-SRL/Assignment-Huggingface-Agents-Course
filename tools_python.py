1f17929a4bd511ec1d35781c37fa63e6cbd7eb47
Marin Iuga <marin.iuga@intertechnica.com>
2025-05-15 13:42:27 +0300

    Adding file headers.

--- tools_python.py
+++ tools_python.py
@@ -1,3 +1,7 @@
+# Copyright (c) Iuga Marin
+# This file is part of the HuggingFace free AI Agents course assignment.
+# It contains utility functions for reading Python script files from the GAIA dataset.
+
 from tools_hfhub import get_GAIA_dataset_file
 
 def get_python_file_data(file_name: str) -> str:
